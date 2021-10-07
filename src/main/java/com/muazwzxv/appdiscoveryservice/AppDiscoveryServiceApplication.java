690f270c1d66be526afaadc2c58b6a68338dc242
Cilantroz <muazwazir@gmail.com>
2021-10-08 03:47:35 +0800

    config eureka server

--- src/main/java/com/muazwzxv/appdiscoveryservice/AppDiscoveryServiceApplication.java
+++ src/main/java/com/muazwzxv/appdiscoveryservice/AppDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.muazwzxv.appdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AppDiscoveryServiceApplication {
 
     public static void main(String[] args) {
