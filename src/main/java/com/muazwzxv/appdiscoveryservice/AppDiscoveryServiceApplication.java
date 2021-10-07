30bbe34a3766cb641c4fcd7cbd66dbb001be23dd
Cilantroz <muazwazir@gmail.com>
2021-10-08 03:17:12 +0800

    code dump

---
+++ src/main/java/com/muazwzxv/appdiscoveryservice/AppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muazwzxv.appdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDiscoveryServiceApplication.class, args);
+    }
+
+}
