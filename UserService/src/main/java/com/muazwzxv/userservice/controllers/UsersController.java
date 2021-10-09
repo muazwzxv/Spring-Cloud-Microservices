e9d9a86a479213942f1891969c0220948a169a6d
Cilantroz <muazwazir@gmail.com>
2021-10-10 00:21:42 +0800

    redo user service

--- UserService/src/main/java/com/muazwzxv/userservice/controllers/UsersController.java
+++ UserService/src/main/java/com/muazwzxv/userservice/controllers/UsersController.java
@@ -4,13 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UsersController {
 
     @GetMapping("/status/check")
     public String status() {
-        return "Server is working";
+       return "this endpoint is working";
     }
 }
