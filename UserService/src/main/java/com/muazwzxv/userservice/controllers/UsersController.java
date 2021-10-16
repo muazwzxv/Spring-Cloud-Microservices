e1f83b62e6cad6618b7ea3ea3ff6b0c76aa5d0f4
Cilantroz <muazwazir@gmail.com>
2021-10-16 20:21:46 +0800

    add spring jpa support and some

--- UserService/src/main/java/com/muazwzxv/userservice/controllers/UsersController.java
+++ UserService/src/main/java/com/muazwzxv/userservice/controllers/UsersController.java
@@ -1,10 +1,9 @@
 package com.muazwzxv.userservice.controllers;
 
+import com.muazwzxv.userservice.models.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -16,5 +15,10 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
        return "Status check from User Service on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String create(@RequestBody Users user) {
+        return "create user method is called";
     }
 }
---
+++ UserService/src/main/java/com/muazwzxv/userservice/models/Users.java
@@ -0,0 +1,75 @@
+package com.muazwzxv.userservice.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="users")
+public class Users implements Serializable {
+
+    // Private user id
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    private String password;
+
+    // Public user id exposed to the user
+    private String userId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
