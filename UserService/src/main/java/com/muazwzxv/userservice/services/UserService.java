59123346598756a72d99a982d85cc0649bb5b1e5
Cilantroz <muazwazir@gmail.com>
2021-10-21 00:07:44 +0800

    bcrypt bean

--- UserService/src/main/java/com/muazwzxv/userservice/services/UserService.java
+++ UserService/src/main/java/com/muazwzxv/userservice/services/UserService.java
@@ -3,16 +3,19 @@ package com.muazwzxv.userservice.services;
 import com.muazwzxv.userservice.models.Users;
 import com.muazwzxv.userservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bcrypt;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bcrypt) {
         this.userRepository = userRepository;
+        this.bcrypt = bcrypt;
     }
 
     public Users createUser(Users user)  {
