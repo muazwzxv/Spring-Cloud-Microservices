b2b3d190a7ae55664cfac73c045e471a9b621417
Muashh <cilantroz@pop-os.localdomain>
2021-11-23 17:20:30 +0800

    config server

---
+++ ApiConfigServer/src/test/java/com/muazwzxv/apiconfigserver/ApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muazwzxv.apiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- ApiGateway/src/main/java/com/muazwzxv/apigateway/filters/GlobalPostFilter.java
+++ ApiGateway/src/main/java/com/muazwzxv/apigateway/filters/GlobalPostFilter.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class GlobalPostFilter implements GlobalFilter {
+public class GlobalPostFilter implements GlobalFilter, Ordered {
 
     final Logger logger = LoggerFactory.getLogger(GlobalPostFilter.class);
 
@@ -21,5 +22,10 @@ public class GlobalPostFilter implements GlobalFilter {
                     // Logic resides here
                     logger.info("Global post filter is executed");
                 }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
